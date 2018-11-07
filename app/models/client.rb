3d2dba0604bf9ac97348dd0c4360de05dd68022a
Ana Paola Treviño <anapao.trevino@gmail.com>
2018-11-06 22:41:00 -0600

    missing associations

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,5 @@ class Client < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :trips
 end
