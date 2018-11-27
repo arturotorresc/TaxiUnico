f8b541b8ba470d29f5a890f824aeb0efef43ee15
Alberto <aji147@gmail.com>
2018-11-27 10:40:47 -0600

    Admin dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,13 @@ class ApplicationController < ActionController::Base
 			devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :location)}
 			devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :location)}
 		end
+  def flash_class(level)
+	  case level
+	    when 'notice' then "alert alert-info"
+	    when 'success' then "alert alert-success"
+	    when 'error' then "alert alert-danger"
+	    when 'alert' then "alert alert-warning"
+	  end
+	end
 
 end
